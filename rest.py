15490a748b33295072a4b76c090c7d5994f4f9ff
CV48LU <dragos.stefanescu@ing.com>
2022-05-05 19:45:19 +0300

    ret sample added

--- rest.py
+++ rest.py
@@ -1,8 +1,26 @@
 import requests, json
 
-# check this: https://mixedanalytics.com/blog/list-actually-free-open-no-auth-needed-apis/
 
-url = "https://api.coinbase.com/v2/currencies"
+url = "https://api.punkapi.com/v2/beers" #"https://api.coinbase.com/v2/currencies"
 response = requests.request("GET", url)
-json_response = json.loads(response.text)          # this is traversable now
-print( json_response)
+json_response = json.loads(response.content)          # this is traversable now
+# print( json_response)
+mx_ph =  float( json_response[0]["ph"])
+mx_name  =  json_response[0]["name"]
+mn_ph =   float( json_response[0]["ph"])
+mn_name  =  json_response[0]["name"]
+
+for  value in json_response:
+        name = value["name"]
+        ph = value["ph"]
+        if (ph):
+            if (mn_ph < float(ph)): 
+                mn_name = name
+                mn_ph = float(ph)
+            if (mx_ph > float(ph)): 
+                mx_name = name
+                mx_ph = float(ph)
+                
+print("name = ",mx_name, " ph=", mx_ph)
+print("name = ",mn_name, " ph=", mn_ph)
+
