e07cdac2a080a06cac4f303fabe790bb0493cf87
CV48LU <dragos.stefanescu@ing.com>
2022-05-05 17:31:16 +0300

    first 2

---
+++ f1.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python3
+
+import sys
+
+a = sys.argv[1]
+b = sys.argv[2]
+x = input("x value: ") 
+fx = int(a) * int(x) + int(b)
+
+print( "f(x)=" , fx)
---
+++ rand.py
@@ -0,0 +1,9 @@
+import random
+
+nr = random.randint(3, 9)
+print( nr)
+
+if nr % 2 ==0:
+    print( "Even")
+else:
+    print( "Odd")
